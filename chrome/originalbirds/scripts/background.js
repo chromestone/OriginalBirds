c4f2644078458f062f572b6a8e3f81d178df08e8
Derek Zhang <chromestone@users.noreply.github.com>
2023-04-22 14:19:51 -0400

    breakthrough with checkmarks

--- chrome/originalbirds/scripts/background.js
+++ chrome/originalbirds/scripts/background.js
@@ -1,3 +1,15 @@
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+	if (msg.text == "tab_id?") {
+		sendResponse({tab: sender.tab.id});
+	}
+});
+
+chrome.tabs.create({url: "https://twitter.com/elonmusk"}, function(tab) {
+
+	console.log(tab.id);
+	chrome.storage.local.set({closeme : tab.id});
+});
+
 async function loadHandles() {
 
 	const response = await fetch("../data/verified_handles.txt");
@@ -5,12 +17,43 @@ async function loadHandles() {
 	const handles = data.split('\n');
 
 	const handlesSet = new Set(handles);
-	console.log(handlesSet.has('taylorswift13'));
-	console.log(handlesSet.has('"taylorswift13"'));
-	// console.log(handlesSet.size);
-	// const thingy = {handlesSet: handlesSet};
-	// console.log(thingy.handlesSet.size);
-	chrome.storage.local.set({handles : JSON.stringify([...handlesSet])});
+	chrome.storage.local.set({handles : [...handlesSet]});
 }
 
 loadHandles();
+
+/*
+// declare a function to open a new tab and find an element on that page
+function retrieveCheckmark() {
+
+	// return new Promise((resolve) => {
+
+		// create a new tab
+		chrome.tabs.create({url: "https://twitter.com/elonmusk"}, function(tab) {
+
+			console.log(tab.id);
+			chrome.storage.local.set({closeme : tab.id});
+		});
+		/*
+			// wait for the tab to load
+			chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+				// check if the tab has finished loading and is active
+				if (tabId === tab.id && changeInfo.status === "complete" && tab.active) {
+
+					// find an element with class "example-element"
+					chrome.tabs.executeScript(tab.id, {code: "document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-1pos5eu.r-qvutc0 > span > span > div:nth-child(1)').outerHTML"},
+						function(result) {
+							// log the result to the console
+							console.log(result);
+					});
+				}
+				else {
+
+					//TODO
+					;
+				}
+			});
+	// });
+	retrieveCheckmark();
+}*/
--- chrome/originalbirds/scripts/content.js
+++ chrome/originalbirds/scripts/content.js
@@ -11,70 +11,122 @@ function waitForElement(selector) {
 	});
 }
 
-waitForElement('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span').then((targetElement) => {
-	// Do something with targetElement
+function setCheckmark(targetElement) {
+
 	console.log(targetElement);
+	chrome.storage.local.set({checkmark : targetElement.outerHTML});
+	window.close();
+}
+
+function verifyHandles(handles) {
+
+	return new Promise((resolve) => {
+
+		chrome.storage.local.get("handles", function(result) {
+
+			const handlesSet = new Set(result.handles);
+			resolve(handles.map(element => handlesSet.has(element)));
+		});
+	});
+}
+
+function retrieveCheckmark() {
+
+	return new Promise((resolve) => {
+
+		chrome.storage.local.get("checkmark", function(result) {
+
+			resolve(result.checkmark);
+		});
+	});
+}
+
+/*
+function retrieveCheckmark() {
+
+	return new Promise((resolve) => {
+
+		// create a new iFrame element
+		var iframe = document.createElement('iframe');
+
+		// hide the iFrame by setting its display property to 'none'
+		iframe.style.display = 'none';
+
+		// set the iFrame's source to the URL of the page with the dynamically loaded data
+		iframe.src = 'https://twitter.com/elonmusk';
+
+		// append the iFrame to the current page's document body
+		document.body.appendChild(iframe);
+
+		// wait for the iFrame to load
+		iframe.onload = function() {
+
+			// access the content document of the iFrame
+			var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+			// query for an element using a CSS selector
+			var elem = iframeDoc.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-1pos5eu.r-qvutc0 > span > span > div:nth-child(1)");
+
+			// do something with the element
+			if (elem) {
+
+				resolve(elem.outerHTML);
+			}
+			else {
+
+				resolve(null);
+			}
+		};
+	});
+}
+*/
+
+async function verifyUserPage(targetElement) {
+
 	const url = new URL(window.location);
 	const handle = location.pathname.split('/')[1];
-	console.log(handle);
 	console.log(targetElement.firstChild.textContent);
-	chrome.storage.local.get("handles", function(result){
 
-		const handlesSet = new Set(JSON.parse(result.handles));
-		console.log(handlesSet.size);
-		console.log(handlesSet.has(handle));
-		// console.log(result.handlesSet.has(handle));
+	const verified = await verifyHandles([handle]);
+	console.log(verified);
+	if (verified[0]) {
+
+		console.log("verified!");
+		const result = await retrieveCheckmark();
+		console.log(result);
+
+		//const test = await retrieveCheckmark();
+		/*
+		const check_html = await retrieveCheckmark();
+		console.log(check_html);
+		if (check_html) {
+
+			var div = document.createElement('div');
+			div.innerHTML = check_html.trim();
+		}*/
+	}
+}
+
+chrome.runtime.sendMessage({ text: "tab_id?" }, tabObj => {
+	//console.log('My tabId is', tabId);
+	chrome.storage.local.get("closeme", function(result) {
+
+		console.log(tabObj.tab);
+		console.log(result.closeme);
+		if (tabObj.tab == result.closeme) {
+
+			waitForElement('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-1pos5eu.r-qvutc0 > span > span > div:nth-child(1)').then(setCheckmark);
+		}
+		else {
+
+			waitForElement('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span').then(verifyUserPage);
+		}
 	});
 });
 
-/*
-// Create a MutationObserver instance
-const observer = new MutationObserver((mutationsList, observer) => {
-	// Iterate over the list of mutations
-	for (const mutation of mutationsList) {
-		// Check if any of the added nodes match the target element
-		if (mutation.addedNodes) {
-			for (const node of mutation.addedNodes) {
-				if (node.matches && node.matches('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span')) {
-					// Do something when the target element is loaded
-					console.log('Target element loaded!');
-
-					const url = new URL(window.location);
-					console.log(location.pathname.split('/')[1]);
-					console.log(node.firstChild.textContent);
-
-					// Disconnect the observer to stop observing
-					observer.disconnect();
-				}
-			}
-		}
-	}
-});
-
-// Start observing the document for mutations
-observer.observe(document.documentElement, {
-	childList: true,
-	subtree: true,
-	attributes: true
-});
-*/
-// console.log(document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span"));
-
-
-// Add an event listener for the window.onload event
-// window.onload = () => {
-//   // Run your extension code here
-//   console.log('Page has fully loaded!');
-//   console.log(document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span"));
-// };
-
-
-// const nametag = document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span");
-
-// console.log(nametag);
-// if (nametag) {
-
-// 	const url = new URL(window.location);
-// 	console.log(location.pathname.split('/')[1]);
-// 	console.log(nametag.firstChild.textContent);
-// }
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//     var myTabId = tabs[0].id;
+//     chrome.tabs.sendMessage(myTabId, {text: "hi"}, function(response) {
+//         alert(response);
+//     });
+// });
