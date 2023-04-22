b350f6feb47808bdb8c0ee2027374fcb28ea8d5d
Derek Zhang <chromestone@users.noreply.github.com>
2023-04-22 14:55:51 -0400

    checkmark works
    first working commit. woohoo!

--- chrome/originalbirds/scripts/background.js
+++ chrome/originalbirds/scripts/background.js
@@ -21,39 +21,3 @@ async function loadHandles() {
 }
 
 loadHandles();
-
-/*
-// declare a function to open a new tab and find an element on that page
-function retrieveCheckmark() {
-
-	// return new Promise((resolve) => {
-
-		// create a new tab
-		chrome.tabs.create({url: "https://twitter.com/elonmusk"}, function(tab) {
-
-			console.log(tab.id);
-			chrome.storage.local.set({closeme : tab.id});
-		});
-		/*
-			// wait for the tab to load
-			chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-				// check if the tab has finished loading and is active
-				if (tabId === tab.id && changeInfo.status === "complete" && tab.active) {
-
-					// find an element with class "example-element"
-					chrome.tabs.executeScript(tab.id, {code: "document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-1pos5eu.r-qvutc0 > span > span > div:nth-child(1)').outerHTML"},
-						function(result) {
-							// log the result to the console
-							console.log(result);
-					});
-				}
-				else {
-
-					//TODO
-					;
-				}
-			});
-	// });
-	retrieveCheckmark();
-}*/
--- chrome/originalbirds/scripts/content.js
+++ chrome/originalbirds/scripts/content.js
@@ -41,46 +41,6 @@ function retrieveCheckmark() {
 	});
 }
 
-/*
-function retrieveCheckmark() {
-
-	return new Promise((resolve) => {
-
-		// create a new iFrame element
-		var iframe = document.createElement('iframe');
-
-		// hide the iFrame by setting its display property to 'none'
-		iframe.style.display = 'none';
-
-		// set the iFrame's source to the URL of the page with the dynamically loaded data
-		iframe.src = 'https://twitter.com/elonmusk';
-
-		// append the iFrame to the current page's document body
-		document.body.appendChild(iframe);
-
-		// wait for the iFrame to load
-		iframe.onload = function() {
-
-			// access the content document of the iFrame
-			var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-			// query for an element using a CSS selector
-			var elem = iframeDoc.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-1pos5eu.r-qvutc0 > span > span > div:nth-child(1)");
-
-			// do something with the element
-			if (elem) {
-
-				resolve(elem.outerHTML);
-			}
-			else {
-
-				resolve(null);
-			}
-		};
-	});
-}
-*/
-
 async function verifyUserPage(targetElement) {
 
 	const url = new URL(window.location);
@@ -92,18 +52,20 @@ async function verifyUserPage(targetElement) {
 	if (verified[0]) {
 
 		console.log("verified!");
-		const result = await retrieveCheckmark();
-		console.log(result);
 
-		//const test = await retrieveCheckmark();
-		/*
 		const check_html = await retrieveCheckmark();
 		console.log(check_html);
 		if (check_html) {
 
-			var div = document.createElement('div');
+			var div = document.createElement('span');
 			div.innerHTML = check_html.trim();
-		}*/
+			var svg = div.querySelector('svg');
+			if (svg) {
+
+				svg.style.color = "#800080";
+			}
+			targetElement.appendChild(div);
+		}
 	}
 }
 
@@ -123,10 +85,3 @@ chrome.runtime.sendMessage({ text: "tab_id?" }, tabObj => {
 		}
 	});
 });
-
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//     var myTabId = tabs[0].id;
-//     chrome.tabs.sendMessage(myTabId, {text: "hi"}, function(response) {
-//         alert(response);
-//     });
-// });
