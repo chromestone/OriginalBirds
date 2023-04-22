b442f9013d145ad53673fc5d4ac7f623a16d9e8a
Derek Zhang <chromestone@users.noreply.github.com>
2023-04-21 22:56:08 -0400

    baby steps
    prints the twitter handle and account name

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OriginalBirds
 Where my OBs at?
+
+This browser extension brings back the legacy Twitter verified status.
+
+_(Please don't sue me Elon.)_
---
+++ chrome/originalbirds/scripts/content.js
@@ -0,0 +1,72 @@
+function waitForElement(selector) {
+	return new Promise((resolve) => {
+		const observer = new MutationObserver((mutations) => {
+			const targetElement = document.querySelector(selector);
+			if (targetElement) {
+				observer.disconnect();
+				resolve(targetElement);
+			}
+		});
+		observer.observe(document.body, { childList: true, subtree: true });
+	});
+}
+
+waitForElement('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span').then((targetElement) => {
+	// Do something with targetElement
+	console.log(targetElement);
+	const url = new URL(window.location);
+	console.log(location.pathname.split('/')[1]);
+	console.log(targetElement.firstChild.textContent);
+});
+
+/*
+// Create a MutationObserver instance
+const observer = new MutationObserver((mutationsList, observer) => {
+	// Iterate over the list of mutations
+	for (const mutation of mutationsList) {
+		// Check if any of the added nodes match the target element
+		if (mutation.addedNodes) {
+			for (const node of mutation.addedNodes) {
+				if (node.matches && node.matches('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span')) {
+					// Do something when the target element is loaded
+					console.log('Target element loaded!');
+
+					const url = new URL(window.location);
+					console.log(location.pathname.split('/')[1]);
+					console.log(node.firstChild.textContent);
+
+					// Disconnect the observer to stop observing
+					observer.disconnect();
+				}
+			}
+		}
+	}
+});
+
+// Start observing the document for mutations
+observer.observe(document.documentElement, {
+	childList: true,
+	subtree: true,
+	attributes: true
+});
+*/
+// console.log(document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span"));
+
+
+// Add an event listener for the window.onload event
+// window.onload = () => {
+//   // Run your extension code here
+//   console.log('Page has fully loaded!');
+//   console.log(document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span"));
+// };
+
+
+// const nametag = document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span");
+
+// console.log(nametag);
+// if (nametag) {
+
+// 	const url = new URL(window.location);
+// 	console.log(location.pathname.split('/')[1]);
+// 	console.log(nametag.firstChild.textContent);
+// }
