922c22d4a909036e6ac9a18fc1ae6d66a009e2be
Derek Zhang <chromestone@users.noreply.github.com>
2023-04-24 20:31:48 -0400

    major refactoring
    generalized checkmark adding code into one class

--- chrome/originalbirds/scripts/content.js
+++ chrome/originalbirds/scripts/content.js
@@ -32,11 +32,6 @@ const SPAN_WITH_ID = 'span[id]';
 
 // const CHECKMARK_TOOLTIP = "A legacy verified user used this Twitter handle in the past.";
 
-function myRandomId() {
-
-	return "id_" + Date.now().toString() + Math.random().toString(16).slice(2);
-}
-
 function waitForElement(selector) {
 	return new Promise((resolve) => {
 		const observer = new MutationObserver((mutations) => {
@@ -57,7 +52,7 @@ function setCheckmark(targetElement) {
 	window.close();
 }
 
-function retrieveCheckmark() {
+function getCheckmark() {
 
 	return new Promise((resolve) => {
 
@@ -68,223 +63,22 @@ function retrieveCheckmark() {
 	});
 }
 
-function getUserPageObserver() {
+function getVerifiedHandles() {
 
-	var FEEDS_PROCESSED = new Set();
+	return new Promise((resolve) => {
 
-	return async () => {
+		chrome.storage.local.get("handles", (result) => {
 
-		const handleElement = document.querySelector(USER_SELECTOR);
-		if (handleElement === null) {
-
-			return;
-		}
-
-		//const verified = await verifiedHandles([handleElement.textContent?.substring(1)]);
-		if (typeof window.originalBirdsHandlesSet === 'undefined') {
-
-			return;
-		}
-
-		const verified = window.originalBirdsHandlesSet.has(handleElement.textContent?.substring(1));
-		if (!verified) {
-
-			return;
-		}
-
-		const checkHtml = await retrieveCheckmark();
-		if (checkHtml === null) {
-
-			return;
-		}
-
-		const parent = handleElement.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.
-			parentElement?.parentElement;
-		const targetElement = parent?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild?.
-			firstElementChild?.lastElementChild;
-		// check undefined as well
-		if (parent == null || targetElement == null) {
-
-			console.log("This should not happen.");
-		}
-		else {
-
-			let checkmarkFound = false;
-			for (const child of targetElement.children) {
-
-				if (FEEDS_PROCESSED.has(child.id)) {
-
-					checkmarkFound = true;
-					break;
-				}
-			}
-			if (!checkmarkFound) {
-
-				let myId = myRandomId();
-				while (FEEDS_PROCESSED.has(myId)) {
-
-					myId = myRandomId();
-				}
-				FEEDS_PROCESSED.add(myId);
-
-				const div = document.createElement("span");
-
-				//div.classList.add("tooltip");
-
-				div.id = myId;
-				// div.style.verticalAlign = "middle";
-
-				div.innerHTML = checkHtml;
-				const svg = div.querySelector('svg');
-				if (svg !== null) {
-
-					svg.style.color = "#2DB32D";//"#800080";
-				}
-
-				//const tooltipSpan = document.createElement("span");
-				//tooltipSpan.classList.add('tooltiptext');
-				//tooltipSpan.textContent = CHECKMARK_TOOLTIP;
-				//div.appendChild(tooltipSpan);
-
-				targetElement.appendChild(div);
-			}
-		}
-
-		const headingElement = document.querySelector(HEADING_SELECTOR);
-		if (headingElement === null) {
-
-			return;
-		}
-
-		let checkmarkFound = false;
-		for (const child of headingElement.children) {
-
-			if (FEEDS_PROCESSED.has(child.id)) {
-
-				checkmarkFound = true;
-				break;
-			}
-		}
-		if (!checkmarkFound) {
-
-			let myId = myRandomId();
-			while (FEEDS_PROCESSED.has(myId)) {
-
-				myId = myRandomId();
-			}
-			FEEDS_PROCESSED.add(myId);
-
-			const div = document.createElement("span");
-
-			div.id = myId;
-			div.style.display = "flex";
-		
-			div.innerHTML = checkHtml;
-			const svg = div.querySelector('svg');
-			if (svg !== null) {
-		
-				svg.style.color = "#2DB32D";
-			}
-
-			headingElement.appendChild(div);
-		}
-	};
-}
-
-function getFeedObserver(selector) {
-
-	var FEEDS_PROCESSED = new Set();
-
-	return () => {
-
-		const theDate = Date.now();
-		const targetElements = [...document.querySelectorAll(selector)];
-		console.log(Date.now() - theDate);
-
-		if (targetElements.length == 0) {
-
-			return;
-		}
-
-		//const theDate2 = Date.now();
-		//verifiedHandles(targetElements.map((element) => element.textContent?.substring(1))).then((verified) => {
-		if (typeof window.originalBirdsHandlesSet === 'undefined') {
-
-			return;
-		}
-
-		const doProcessing = targetElements.filter(element => window.originalBirdsHandlesSet.has(element.textContent?.substring(1)));
-		//console.log(Date.now() - theDate2);
-
-		//const doProcessing = targetElements.filter((_, idx) => verified[idx]);
-
-		if (doProcessing.length == 0) {
-
-			return;
-		}
-
-		retrieveCheckmark().then((checkHtml) => {
-
-			if (checkHtml === null) {
-
-				return;
-			}
-
-			for (const element of doProcessing) {
-
-				const parent = element.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.
-					parentElement;
-				const targetElement = parent?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild;
-				// check undefined as well
-				if (parent == null || targetElement == null) {
-
-					console.log("This should not happen.");
-					continue;
-				}
-
-				//console.log(targetElement);
-				let checkmarkFound = false;
-				for (const child of targetElement.children) {
-
-					//console.log(child.id);
-					if (FEEDS_PROCESSED.has(child.id)) {
-
-						checkmarkFound = true;
-						break;
-					}
-				}
-				if (checkmarkFound) {
-
-					continue;
-				}
-
-				let myId = myRandomId();
-				while (FEEDS_PROCESSED.has(myId)) {
-
-					myId = myRandomId();
-				}
-				FEEDS_PROCESSED.add(myId);
-				//console.log(myId);
-
-				const div = document.createElement("span");
-
-				div.id = myId;
-				div.style.display = "flex";
-
-				div.innerHTML = checkHtml;
-				const svg = div.querySelector('svg');
-				if (svg !== null) {
-
-					svg.style.color = "#2DB32D";
-				}
-
-				targetElement.appendChild(div);
-			}
+			resolve(typeof result.handles === 'undefined' ? null : new Set(result.handles));
 		});
-		//});
-	};
+	});
 }
 
+function myRandomId() {
+
+	return "id_" + Date.now().toString() + "_" + Math.random().toString(16).slice(2);
+}
+/*
 function getHoverObserver() {
 
 	var FEEDS_PROCESSED = new Set();
@@ -460,31 +254,223 @@ function getRecommendedObserver() {
 		//});
 	};
 }
+*/
+class CheckmarkManager {
 
-function registerRecurringObserver() {
+	constructor(verifiedHandles, checkHtml) {
+
+		this.verifiedHandles = verifiedHandles;
+		this.checkHtml = checkHtml;
+		this.checkmarkIds = new Set();
+	}
+
+	updateUserPage(user_selector, heading_selector) {
+
+		const handleElement = document.querySelector(user_selector);
+		if (handleElement === null || !this.verifiedHandles.has(handleElement.textContent?.substring(1))) {
+
+			return;
+		}
+
+		const parent = handleElement.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.
+			parentElement?.parentElement;
+		const targetElement = parent?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild?.
+			firstElementChild?.lastElementChild;
+		// this should not happen unless html structure changed
+		// double equal checks for undefined as well
+		if (parent == null || targetElement == null) {
+
+			console.log("Error: Original Birds could not locate checkmark parent.");
+		}
+		else {
+
+			let checkmarkFound = false;
+			for (const child of targetElement.children) {
+
+				if (this.checkmarkIds.has(child.id)) {
+
+					checkmarkFound = true;
+					break;
+				}
+			}
+			if (!checkmarkFound) {
+
+				let myId = myRandomId();
+				while (this.checkmarkIds.has(myId)) {
+
+					myId = myRandomId();
+				}
+				this.checkmarkIds.add(myId);
+
+				const div = document.createElement("span");
+
+				//div.classList.add("tooltip");
+
+				div.id = myId;
+				// div.style.verticalAlign = "middle";
+
+				div.innerHTML = this.checkHtml;
+				const svg = div.querySelector('svg');
+				if (svg !== null) {
+
+					svg.style.color = "#2DB32D";//"#800080";
+				}
+
+				//const tooltipSpan = document.createElement("span");
+				//tooltipSpan.classList.add('tooltiptext');
+				//tooltipSpan.textContent = CHECKMARK_TOOLTIP;
+				//div.appendChild(tooltipSpan);
+
+				targetElement.appendChild(div);
+			}
+		}
+
+		const headingElement = document.querySelector(heading_selector);
+		if (headingElement === null) {
+
+			return;
+		}
+
+		for (const child of headingElement.children) {
+
+			if (this.checkmarkIds.has(child.id)) {
+
+				return;
+			}
+		}
+
+		let myId = myRandomId();
+		while (this.checkmarkIds.has(myId)) {
+
+			myId = myRandomId();
+		}
+		this.checkmarkIds.add(myId);
+
+		const div = document.createElement("span");
+
+		div.id = myId;
+		div.style.display = "flex";
+
+		div.innerHTML = this.checkHtml;
+		const svg = div.querySelector('svg');
+		if (svg !== null) {
+	
+			svg.style.color = "#2DB32D";
+		}
+
+		headingElement.appendChild(div);
+	}
+
+	updateCheckmark(selector, element2Target) {
+
+		for (const element of document.querySelectorAll(selector)) {
+
+			if (!this.verifiedHandles.has(element.textContent?.substring(1))) {
+
+				continue;
+			}
+
+			//const parent = element.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.
+			//	parentElement;
+			//const targetElement = parent?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild;
+			//const parent = element2Parent(element);
+			const targetElement = element2Target(element);//parent2Target(parent);
+			// this should not happen unless html structure changed
+			// double equal checks for undefined as well
+			if (targetElement == null) {
+
+				console.log("Error: Original Birds could not locate checkmark parent.");
+				continue;
+			}
+
+			let checkmarkFound = false;
+			for (const child of targetElement.children) {
+
+				if (this.checkmarkIds.has(child.id)) {
+
+					checkmarkFound = true;
+					break;
+				}
+			}
+			if (checkmarkFound) {
+
+				continue;
+			}
+
+			let myId = myRandomId();
+			while (this.checkmarkIds.has(myId)) {
+
+				myId = myRandomId();
+			}
+			this.checkmarkIds.add(myId);
+
+			const div = document.createElement("span");
+
+			div.id = myId;
+			div.style.display = "flex";
+
+			div.innerHTML = this.checkHtml;
+			const svg = div.querySelector('svg');
+			if (svg !== null) {
+
+				svg.style.color = "#2DB32D";
+			}
+
+			targetElement.appendChild(div);
+		}
+	}
+}
+
+async function registerRecurringObserver() {
+
+	const verifiedHandles = await getVerifiedHandles();
+	if (verifiedHandles === null) {
+
+		console.log("Warning: Original Birds could not load verified handles.");
+		return;
+	}
+	const checkHtml = await getCheckmark();
+	if (checkHtml === null) {
+
+		console.log("Warning: Original Birds could not load checkmark.");
+		return;
+	}
+
+	const manager = new CheckmarkManager(verifiedHandles, checkHtml);
 
 	var invocations = 10;
 
-	const updateUserPage = getUserPageObserver();
-	const updateFeed = getFeedObserver(FEED_SELECTOR);
-	const updateReplyPost = getFeedObserver(THREAD_REPLY_POST_SELECTOR);
-	const updateHoverCard = getHoverObserver();
-	const updateRecommended = getRecommendedObserver();
+	//const updateUserPage = getUserPageObserver();
+	//const updateFeed = getFeedObserver(FEED_SELECTOR);
+	//const updateReplyPost = getFeedObserver(THREAD_REPLY_POST_SELECTOR);
+	//const updateHoverCard = getHoverObserver();
+	//const updateRecommended = getRecommendedObserver();
 
 	function addCheckmark() {
 
+		console.log(invocations);
 		if (invocations > 0) {
 
 			invocations -= 1;
 
-			updateUserPage();
-			updateFeed();
-			updateReplyPost();
-			updateHoverCard();
-			updateRecommended();
+			manager.updateUserPage(USER_SELECTOR, HEADING_SELECTOR);
+			manager.updateCheckmark(FEED_SELECTOR,
+				(element) => element.closest('div[data-testid="User-Name"]')?.
+					firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild);
+			manager.updateCheckmark(THREAD_REPLY_POST_SELECTOR,
+				(element) => element.closest('div[data-testid="User-Name"]')?.
+					firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild);
+			manager.updateCheckmark(HOVER_CARD_SELECTOR,
+				(element) => element.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.
+					firstElementChild?.firstElementChild?.lastElementChild);
+			manager.updateCheckmark(RECOMMENDATION_SELECTOR,
+				(element) => element.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.
+					parentElement?.
+					firstElementChild?.firstElementChild?.lastElementChild);
 
 			window.setTimeout(addCheckmark, 500);
 		}
+		console.log("stopped");
 	}
 	addCheckmark();
 
@@ -547,11 +533,6 @@ chrome.runtime.sendMessage({ text: "tab_id?" }, response => {
 		else {
 
 			//createTooltipCSS();
-			//registerOneTimeObserver();
-			chrome.storage.local.get("handles", (result) => {
-
-				window.originalBirdsHandlesSet = new Set(typeof result.handles === 'undefined' ? [] : result.handles);
-			});
 			registerRecurringObserver();
 		}
 	});
